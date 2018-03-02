c65f086d2fb6fa0daa59b9fa7d0894cde8ec159b
eder-toledo <me890506@gmail.com>
2018-03-01 21:23:47 -0600

    Se modifico para dejar las primary key como autoincrementable

--- database.sql
+++ database.sql
@@ -1,136 +1,259 @@
-CREATE TABLE catalogSurveyType
-(
- idCatalogSurveyType INT NOT NULL ,
- name                VARCHAR(50) NOT NULL ,
+-- MySQL Workbench Forward Engineering
 
- CONSTRAINT PK_catalogSurveyType PRIMARY KEY (idCatalogSurveyType ASC)
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE TABLE catalog_choice
-(
- idCatalogChoice INT NOT NULL ,
+-- -----------------------------------------------------
+-- Schema kida
+-- -----------------------------------------------------
 
- CONSTRAINT PK_catalog_choice PRIMARY KEY (idCatalogChoice ASC)
-);
+-- -----------------------------------------------------
+-- Schema kida
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `kida` DEFAULT CHARACTER SET utf8 ;
+USE `kida` ;
 
-CREATE TABLE user
-(
- idUser INT NOT NULL ,
- uuid   CHAR(36) NOT NULL ,
- name   VARCHAR(200) NOT NULL ,
- email  VARCHAR(50) NOT NULL ,
+-- -----------------------------------------------------
+-- Table `kida`.`catalogSurveyType`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`catalogSurveyType` ;
 
- CONSTRAINT PK_user PRIMARY KEY (idUser ASC)
-);
+CREATE TABLE IF NOT EXISTS `kida`.`catalogSurveyType` (
+  `idCatalogSurveyType` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `icon` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`idCatalogSurveyType`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE catalog_surveys
-(
- idCatalogSurvey     INT NOT NULL ,
- typeName                VARCHAR(50) NOT NULL ,
- status              BOOLEAN NOT NULL ,
- idCatalogSurveyType INT NOT NULL ,
 
- CONSTRAINT PK_catalog_surveys PRIMARY KEY (idCatalogSurvey ASC),
- CONSTRAINT FK_204 FOREIGN KEY (idCatalogSurveyType)
-  REFERENCES catalogSurveyType(idCatalogSurveyType)
-);
+-- -----------------------------------------------------
+-- Table `kida`.`catalog_choice`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`catalog_choice` ;
 
-CREATE TABLE form
-(
- idForm        INT NOT NULL ,
- idUser        INT NOT NULL ,
- idFormVersion INT NOT NULL ,
- name          VARCHAR(50) NOT NULL ,
- uuid          CHAR(36) NOT NULL ,
- createdAt     DATETIME NOT NULL ,
- editedAt      DATETIME NOT NULL ,
- deleted       BOOLEAN NOT NULL ,
+CREATE TABLE IF NOT EXISTS `kida`.`catalog_choice` (
+  `idCatalogChoice` INT(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idCatalogChoice`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
- CONSTRAINT PK_form PRIMARY KEY (idForm ASC),
- CONSTRAINT FK_128 FOREIGN KEY (idUser)
-  REFERENCES user(idUser)
-);
 
-CREATE TABLE form_version
-(
- idFormVersion INT NOT NULL ,
- idForm        INT NOT NULL ,
+-- -----------------------------------------------------
+-- Table `kida`.`catalog_surveys`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`catalog_surveys` ;
 
- CONSTRAINT PK_form_version PRIMARY KEY (idFormVersion ASC),
- CONSTRAINT FK_145 FOREIGN KEY (idForm)
-  REFERENCES form(idForm)
-);
+CREATE TABLE IF NOT EXISTS `kida`.`catalog_surveys` (
+  `idCatalogSurvey` INT(11) NOT NULL AUTO_INCREMENT,
+  `typeName` VARCHAR(50) NOT NULL,
+  `status` TINYINT(1) NOT NULL,
+  `catalogSurveyType_idCatalogSurveyType` INT(11) NOT NULL,
+  PRIMARY KEY (`idCatalogSurvey`),
+  INDEX `fk_catalog_surveys_catalogSurveyType1_idx` (`catalogSurveyType_idCatalogSurveyType` ASC),
+  CONSTRAINT `fk_catalog_surveys_catalogSurveyType1`
+    FOREIGN KEY (`catalogSurveyType_idCatalogSurveyType`)
+    REFERENCES `kida`.`catalogSurveyType` (`idCatalogSurveyType`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE response
-(
- idResponse INT NOT NULL ,
- idForm     INT NOT NULL ,
 
- CONSTRAINT PK_response PRIMARY KEY (idResponse ASC),
- CONSTRAINT FK_171 FOREIGN KEY (idForm)
-  REFERENCES form(idForm)
-);
+-- -----------------------------------------------------
+-- Table `kida`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`user` ;
 
-CREATE TABLE permission
-(
- idForm INT NOT NULL ,
+CREATE TABLE IF NOT EXISTS `kida`.`user` (
+  `idUser` INT(11) NOT NULL AUTO_INCREMENT,
+  `uuid` CHAR(36) NOT NULL,
+  `name` VARCHAR(200) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`idUser`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
- CONSTRAINT PK_permission PRIMARY KEY (idForm ASC),
- CONSTRAINT FK_132 FOREIGN KEY (idForm)
-  REFERENCES form(idForm)
-);
 
-CREATE TABLE survey
-(
- idSurvey        INT NOT NULL ,
- idCatalogSurvey INT NOT NULL ,
- idForm          INT NOT NULL ,
- idFormVersion   INT NOT NULL ,
- name            VARCHAR(50) NOT NULL ,
- caption         TEXT NOT NULL ,
- hint            VARCHAR(50) NOT NULL ,
- defaultKey         TEXT NOT NULL ,
- required        BOOLEAN NOT NULL ,
- relevance       VARCHAR(50) NOT NULL ,
- filterKey          VARCHAR(50) NOT NULL ,
- relevanceValue  TEXT NOT NULL ,
- filterValue     TEXT NOT NULL ,
+-- -----------------------------------------------------
+-- Table `kida`.`form`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`form` ;
 
- CONSTRAINT PK_survey PRIMARY KEY (idSurvey ASC),
- CONSTRAINT FK_116 FOREIGN KEY (idCatalogSurvey)
-  REFERENCES catalog_surveys(idCatalogSurvey),
- CONSTRAINT FK_124 FOREIGN KEY (idForm)
-  REFERENCES form(idForm),
- CONSTRAINT FK_153 FOREIGN KEY (idFormVersion)
-  REFERENCES form_version(idFormVersion)
-);
+CREATE TABLE IF NOT EXISTS `kida`.`form` (
+  `idForm` INT(11) NOT NULL AUTO_INCREMENT,
+  `idFormVersion` INT(11) NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `uuid` CHAR(36) NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `editedAt` DATETIME NOT NULL,
+  `deleted` TINYINT(1) NOT NULL,
+  `user_idUser` INT(11) NOT NULL,
+  PRIMARY KEY (`idForm`),
+  INDEX `fk_form_user1_idx` (`user_idUser` ASC),
+  CONSTRAINT `fk_form_user1`
+    FOREIGN KEY (`user_idUser`)
+    REFERENCES `kida`.`user` (`idUser`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE choice
-(
- idChoice        INT NOT NULL ,
- idCatalogChoice INT NOT NULL ,
- idSurvey        INT NOT NULL ,
- name            VARCHAR(50) NOT NULL ,
- caption         TEXT NOT NULL ,
- media           TEXT NOT NULL ,
 
- CONSTRAINT PK_choice PRIMARY KEY (idChoice ASC),
- CONSTRAINT FK_112 FOREIGN KEY (idCatalogChoice)
-  REFERENCES catalog_choice(idCatalogChoice),
- CONSTRAINT FK_120 FOREIGN KEY (idSurvey)
-  REFERENCES survey(idSurvey)
-);
+-- -----------------------------------------------------
+-- Table `kida`.`form_version`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`form_version` ;
 
-CREATE TABLE response_choice
-(
- idResponseChoice INT NOT NULL ,
- idResponse       INT NOT NULL ,
- idChoice         INT NOT NULL ,
- value            JSON NOT NULL ,
+CREATE TABLE IF NOT EXISTS `kida`.`form_version` (
+  `idFormVersion` INT(11) NOT NULL AUTO_INCREMENT,
+  `form_idForm` INT(11) NOT NULL,
+  PRIMARY KEY (`idFormVersion`),
+  INDEX `fk_form_version_form1_idx` (`form_idForm` ASC),
+  CONSTRAINT `fk_form_version_form1`
+    FOREIGN KEY (`form_idForm`)
+    REFERENCES `kida`.`form` (`idForm`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
- CONSTRAINT PK_response_choice PRIMARY KEY (idResponseChoice ASC),
- CONSTRAINT FK_163 FOREIGN KEY (idResponse)
-  REFERENCES response(idResponse),
- CONSTRAINT FK_167 FOREIGN KEY (idChoice)
-  REFERENCES choice(idChoice)
-);
+
+-- -----------------------------------------------------
+-- Table `kida`.`survey`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`survey` ;
+
+CREATE TABLE IF NOT EXISTS `kida`.`survey` (
+  `idSurvey` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `caption` TEXT NOT NULL,
+  `hint` VARCHAR(50) NOT NULL,
+  `defaultKey` TEXT NOT NULL,
+  `required` TINYINT(1) NOT NULL,
+  `relevance` VARCHAR(50) NOT NULL,
+  `filterKey` VARCHAR(50) NOT NULL,
+  `relevanceValue` TEXT NOT NULL,
+  `filterValue` TEXT NOT NULL,
+  `form_idForm` INT(11) NOT NULL,
+  `form_version_idFormVersion` INT(11) NOT NULL,
+  `catalog_surveys_idCatalogSurvey` INT(11) NOT NULL,
+  PRIMARY KEY (`idSurvey`),
+  INDEX `fk_survey_form1_idx` (`form_idForm` ASC),
+  INDEX `fk_survey_form_version1_idx` (`form_version_idFormVersion` ASC),
+  INDEX `fk_survey_catalog_surveys1_idx` (`catalog_surveys_idCatalogSurvey` ASC),
+  CONSTRAINT `fk_survey_form1`
+    FOREIGN KEY (`form_idForm`)
+    REFERENCES `kida`.`form` (`idForm`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_survey_form_version1`
+    FOREIGN KEY (`form_version_idFormVersion`)
+    REFERENCES `kida`.`form_version` (`idFormVersion`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_survey_catalog_surveys1`
+    FOREIGN KEY (`catalog_surveys_idCatalogSurvey`)
+    REFERENCES `kida`.`catalog_surveys` (`idCatalogSurvey`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `kida`.`choice`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`choice` ;
+
+CREATE TABLE IF NOT EXISTS `kida`.`choice` (
+  `idChoice` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `caption` TEXT NOT NULL,
+  `media` TEXT NOT NULL,
+  `survey_idSurvey` INT(11) NOT NULL,
+  `catalog_choice_idCatalogChoice` INT(11) NOT NULL,
+  PRIMARY KEY (`idChoice`),
+  INDEX `fk_choice_survey1_idx` (`survey_idSurvey` ASC),
+  INDEX `fk_choice_catalog_choice1_idx` (`catalog_choice_idCatalogChoice` ASC),
+  CONSTRAINT `fk_choice_survey1`
+    FOREIGN KEY (`survey_idSurvey`)
+    REFERENCES `kida`.`survey` (`idSurvey`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_choice_catalog_choice1`
+    FOREIGN KEY (`catalog_choice_idCatalogChoice`)
+    REFERENCES `kida`.`catalog_choice` (`idCatalogChoice`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `kida`.`permission`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`permission` ;
+
+CREATE TABLE IF NOT EXISTS `kida`.`permission` (
+  `form_idForm` INT(11) NOT NULL,
+  PRIMARY KEY (`form_idForm`),
+  CONSTRAINT `fk_permission_form1`
+    FOREIGN KEY (`form_idForm`)
+    REFERENCES `kida`.`form` (`idForm`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `kida`.`response`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`response` ;
+
+CREATE TABLE IF NOT EXISTS `kida`.`response` (
+  `idResponse` INT(11) NOT NULL AUTO_INCREMENT,
+  `form_idForm` INT(11) NOT NULL,
+  PRIMARY KEY (`idResponse`),
+  INDEX `fk_response_form1_idx` (`form_idForm` ASC),
+  CONSTRAINT `fk_response_form1`
+    FOREIGN KEY (`form_idForm`)
+    REFERENCES `kida`.`form` (`idForm`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `kida`.`response_choice`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kida`.`response_choice` ;
+
+CREATE TABLE IF NOT EXISTS `kida`.`response_choice` (
+  `idResponseChoice` INT(11) NOT NULL AUTO_INCREMENT,
+  `value` JSON NOT NULL,
+  `response_idResponse` INT(11) NOT NULL,
+  `choice_idChoice` INT(11) NOT NULL,
+  PRIMARY KEY (`idResponseChoice`),
+  INDEX `fk_response_choice_response1_idx` (`response_idResponse` ASC),
+  INDEX `fk_response_choice_choice1_idx` (`choice_idChoice` ASC),
+  CONSTRAINT `fk_response_choice_response1`
+    FOREIGN KEY (`response_idResponse`)
+    REFERENCES `kida`.`response` (`idResponse`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_response_choice_choice1`
+    FOREIGN KEY (`choice_idChoice`)
+    REFERENCES `kida`.`choice` (`idChoice`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
